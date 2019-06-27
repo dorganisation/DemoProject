51a93b54d7635e1e3ec50132687964edeb5e5b12
deepak.yadav <deepak.yadav205@gmail.com>
2019-06-27 14:36:23 +0530

    Initial commit first

---
+++ app/src/main/java/quickrvisit/hexlog/admin/demonew/MainActivity.java
@@ -0,0 +1,53 @@
+package quickrvisit.hexlog.admin.demonew;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int i,j,n=5;
+
+        for(i=1;i<=n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++)
+            {
+//                System.out.print("*");
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+
+
+        for(i=1;i<=n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=(2*i-1);j++)
+            {
+
+                if(i%1==0){
+                    System.out.print(i);
+                }else{
+                    System.out.print(i);
+                }
+               // System.out.print(i);
+
+            }
+            System.out.println("");
+        }
+
+    }
+
+
+}
+
